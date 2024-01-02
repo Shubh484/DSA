361325f7933e305c916ad39c75d14dc09a068dba
Shubh <shubhsingh8767@gmail.com>
2024-01-02 20:07:48 +0530

    DSA

---
+++ .vscode/3Sum.cpp
@@ -0,0 +1,106 @@
+// Problem Statement: Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+// Approach - Brute Force
+
+// vector<vector<int>> triplet(int n, vector<int> &arr) {
+//     set<vector<int>> st;
+
+//     // check all possible triplets:
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 if (arr[i] + arr[j] + arr[k] == 0) {
+//                     vector<int> temp = {arr[i], arr[j], arr[k]};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+//                 }
+//             }
+//         }
+//     }
+
+//     //store the set elements in the answer:
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+// Approach - Better Approach (using hashset )
+
+// vector<vector<int>> triplet(int n, vector<int> &arr) {
+//     set<vector<int>> st;
+
+//     for (int i = 0; i < n; i++) {
+//         set<int> hashset;
+//         for (int j = i + 1; j < n; j++) {
+//             //Calculate the 3rd element:
+//             int third = -(arr[i] + arr[j]);
+
+//             //Find the element in the set:
+//             if (hashset.find(third) != hashset.end()) {
+//                 vector<int> temp = {arr[i], arr[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(arr[j]);
+//         }
+//     }
+
+//     //store the set in the answer:
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+// Approach - Optimal approach ( using 3 pointers)
+
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(n, arr);
+    for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
